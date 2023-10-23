924c85fa6988976b43693fd4d4c89d88bfe869b5
gkoberger <gkoberger@gmail.com>
2023-10-23 00:19:32 -0400

    fix pull

--- run/pull.js
+++ run/pull.js
@@ -5,56 +5,37 @@ module.exports = async (repo, branch, data) => {
   var path = require("path");
 
   // Open a repository that needs to be fetched and fast-forwarded
-  return NodeGit.Repository.open(repo)
-    .then(function (repository) {
-      return repository.getBranch("refs/remotes/origin/main");
-    })
-    .then(function (reference) {
-      console.log("Fetched " + reference.name());
-      console.log("and the commit that it currently points to is ");
+  const repository = await NodeGit.Repository.open(repo);
+  const reference = await repository.getBranch("refs/remotes/origin/main");
+  console.log("Fetched " + reference.name());
+  console.log("and the commit that it currently points to is ");
+  const commit = await repository.getBranchCommit(reference.shorthand());
+  console.log(commit.sha());
 
-      return repository.getBranchCommit(reference.shorthand());
-    })
-    .then(function (commit) {
-      console.log(commit.sha());
+  const remoteResult = await repository.getRemote("origin");
+  console.log("Progress:");
 
-      // Get the remote
-      return repository.getRemote("origin");
-    })
-    .then(function (remoteResult) {
-      console.log("Progress:");
+  let remote = remoteResult;
 
-      remote = remoteResult;
+  // Create a fetch options object to get
+  // the fetch progress.
+  var fetchOptions = {
+    callbacks: {
+      credentials: function (url, userName) {
+        return NodeGit.Cred.sshKeyFromAgent(userName);
+      },
+      certificateCheck: function () {
+        return 1;
+      },
+      transferProgress: function (data) {
+        console.log(
+          "(" + data.receivedObjects() + "/" + data.totalObjects() + ")"
+        );
+      },
+    },
+  };
 
-      // Create a fetch options object to get
-      // the fetch progress.
-      var fetchOptions = {
-        callbacks: {
-          credentials: function (url, userName) {
-            return NodeGit.Cred.sshKeyFromAgent(userName);
-          },
-          certificateCheck: function () {
-            return 1;
-          },
-          transferProgress: function (data) {
-            console.log(
-              "(" + data.receivedObjects() + "/" + data.totalObjects() + ")"
-            );
-          },
-        },
-      };
-
-      return remote.fetch(null, fetchOptions);
-    })
-    .then(function () {
-      // Now that we're finished fetching, go ahead and merge our local branch
-      // with the new one
-      return repository.mergeBranches("master", "origin/master");
-    })
-    .catch(function (reason) {
-      console.log(reason);
-    })
-    .done(function () {
-      console.log("Done!");
-    });
+  await remote.fetch(null, fetchOptions);
+  await repository.mergeBranches("main", "origin/main");
+  console.log("Done!");
 };
