83526ddc46a6ffe52c370a1293e941eb013df3c6
Dom Harrington <domharrington@protonmail.com>
2023-10-24 13:48:47 +0100

    Attempt to get a PoC working syncing to gitlab via REST API

--- run/getSidebar.js
+++ run/getSidebar.js
@@ -1,64 +1,32 @@
-const NodeGit = require("../lib/nodegit");
+// https://docs.gitlab.com/ee/api/repositories.html#list-repository-tree
+// curl --header "PRIVATE-TOKEN: xxx" "http://159.223.168.222/api/v4/projects/root%2Ftest-docs/repository/tree?recursive=true" | jq
 
 module.exports = async (repo, branch, data) => {
-  async function listFilesInRepo() {
-    const repository = await NodeGit.Repository.openBare(repo);
-    //const headCommit = await repository.getHeadCommit();
-
-    const references = await NodeGit.Reference.list(repository);
-    console.log(references);
-
-    const refName = `refs/heads/${branch || "main"}`;
-
-    const ref = await NodeGit.Reference.nameToId(repository, refName);
-    const commit = await repository.getCommit(ref);
-
-    const tree = await commit.getTree();
-
-    function convertToSidebar(array) {
-      const result = {};
-
-      array.forEach((path) => {
-        const parts = path.split("/");
-        let currentLevel = result;
-
-        parts.forEach((part, index) => {
-          // If we are at the penultimate part, make the last part an array in the object
-          if (index === parts.length - 2) {
-            if (!currentLevel[part]) {
-              currentLevel[part] = [parts[index + 1]];
-            } else {
-              currentLevel[part].push(parts[index + 1]);
-            }
-          }
-
-          // Else continue building the object
-          else if (!currentLevel[part]) {
-            if (part.match(/md/)) return;
-            currentLevel[part] = {};
-          }
-
-          currentLevel = currentLevel[part];
-        });
-      });
-
-      return result;
+  const sidebar = await fetch(`http://159.223.168.222/api/v4/projects/${encodeURIComponent(repo)}/repository/tree?recursive=true`, {
+    headers: {
+      "PRIVATE-TOKEN": process.env.GITLAB_PRIVATE_TOKEN
     }
+  }).then(res => res.json());
 
-    return new Promise((resolve, reject) => {
-      const filePaths = [];
-      const walker = tree.walk(true);
-      walker.on("entry", (entry) => filePaths.push(entry.path()));
-      walker.on("end", () => resolve(convertToSidebar(filePaths)));
-      walker.on("error", reject);
-      walker.start();
-    });
+  const tree = {}
+
+  for (let index = 0; index < sidebar.length; index++) {
+    const element = sidebar[index];
+    const path = element.path.split('/');
+    path.forEach((section, i) => {
+      if (i === 0) {
+        if (!tree[section]) tree[section] = {}
+      }
+
+      if (i === 1) {
+        if (!tree[path[0]][section]) tree[path[0]][section] = []
+      }
+
+      if (i === 2) {
+        tree[path[0]][path[1]].push(section)
+      }
+    })
   }
 
-  try {
-    const out = await listFilesInRepo();
-    return out;
-  } catch (e) {
-    console.log(e);
-  }
+  return tree;
 };
